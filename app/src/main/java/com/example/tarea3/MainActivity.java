cfb961bfb1374e1fb39670855972a0406fedfddb
Samir <samilr.sg2@gmail.com>
2022-09-28 17:40:55 -0400

    -Se agrego los gestos de izquierda y derecha -Se agrego ajustes al selector de colores

--- app/src/main/java/com/example/tarea3/MainActivity.java
+++ app/src/main/java/com/example/tarea3/MainActivity.java
@@ -10,14 +10,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
 
     EditText txtNumero;
     TextView txtResult;
     FrameLayout layoutInicio;
     Button btnNegro, btnBlanco, btnGris, btnDarkGris;
+    private static final String TAG = "Swipe Position";
+    private float x1, x2, y1, y2;
+    private static int MIN_DISTANCE = 150;
+    private GestureDetector gestureDetector;
 
 
     @Override
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Inicial();
+
+        this.gestureDetector = new GestureDetector(MainActivity.this, this);
     }
 
     public void Inicial() {
@@ -45,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         if (txtNumero.getText().length() < 1){
             txtResult.setText("Por favor ingrese un numero");
         }else {
-
             switch (keyCode){
                 case KeyEvent.KEYCODE_VOLUME_UP:{
                     if (KeyEvent.ACTION_UP==action){
@@ -227,212 +233,103 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.dispatchKeyEvent(event);
     }
+
     public void colorBlanco(View view) {
-            // set the color to relative layout
-            layoutInicio.setBackgroundResource(R.color.white);
-            txtNumero.setTextColor(getResources().getColor(R.color.black));
-            txtResult.setTextColor(getResources().getColor(R.color.black));
-            txtNumero.setBackgroundColor(getResources().getColor(R.color.black));
+        layoutInicio.setBackgroundResource(R.color.white);
+        txtNumero.setTextColor(getResources().getColor(R.color.black));
+        txtResult.setTextColor(getResources().getColor(R.color.black));
+        txtNumero.setBackgroundColor(getResources().getColor(R.color.white));
+        txtNumero.setHintTextColor(getResources().getColor(R.color.black));
     }
     public void colorGris(View view) {
         layoutInicio.setBackgroundResource(R.color.gray);
         txtNumero.setTextColor(getResources().getColor(R.color.black));
         txtResult.setTextColor(getResources().getColor(R.color.black));
-        txtNumero.setBackgroundColor(getResources().getColor(R.color.black));
+        txtNumero.setBackgroundColor(getResources().getColor(R.color.transparent));
+        txtNumero.setHintTextColor(getResources().getColor(R.color.black));
     }
     public void colorDarkGris(View view) {
         layoutInicio.setBackgroundResource(R.color.darkGray);
         txtNumero.setTextColor(getResources().getColor(R.color.white));
         txtResult.setTextColor(getResources().getColor(R.color.white));
+        txtNumero.setHintTextColor(getResources().getColor(R.color.white));
     }
     public void colorNegro(View view) {
         layoutInicio.setBackgroundResource(R.color.black);
         txtNumero.setTextColor(getResources().getColor(R.color.white));
         txtResult.setTextColor(getResources().getColor(R.color.white));
+        txtNumero.setBackgroundColor(getResources().getColor(R.color.black));
+        txtNumero.setHintTextColor(getResources().getColor(R.color.white));
     }
     public void defaultApp(View view) {
         layoutInicio.setBackgroundResource(R.color.colorDefault);
         txtNumero.setTextColor(getResources().getColor(R.color.white));
         txtResult.setTextColor(getResources().getColor(R.color.white));
         txtNumero.setBackgroundColor(getResources().getColor(R.color.transparent));
+        txtNumero.setHintTextColor(getResources().getColor(R.color.white));
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
 
-}
-/*
-    public void calcularCambio(View view) {
-        int billetesCien, billetesCincuenta, billetesVente, monedasDiez, monedasCinco, residuoCien, residuoCiencuenta, residuoVente, residuoDiez;
-        int numeroIntroducido = Integer.parseInt(txtNumero.getText().toString());
-        residuoCien = numeroIntroducido % 100;
-        residuoCiencuenta = residuoCien % 50;
-        residuoVente = residuoCiencuenta % 20;
-        residuoDiez = residuoVente % 10;
-        monedasCinco = residuoDiez / 5;
-        monedasDiez = residuoVente / 10;
-        billetesVente = residuoCiencuenta / 20;
-        billetesCincuenta = residuoCien / 50;
-        billetesCien = numeroIntroducido / 100;
-
-        for (int i = 1; i == 1; i++) {
-            if (numeroIntroducido / 100 >= 1) {
-                if (residuoCien / 50 >= 1){
-                    if (residuoCien / 50 >= 1 && residuoCien % 50 <= 9 && residuoCien % 50 != 0) {
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                        break;
-                    }
-                    if (residuoDiez >= 5 && residuoVente < 10) {
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente)
-                                + " Papeletas de 20\n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                        break;
-                    }
-                    if (residuoCien / 50 >= 1 && residuoCien % 50 >= 10 && residuoCiencuenta < 20) {
-                        if (residuoDiez / 5 >= 1) {
-                            txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasDiez) + " monedas de 10\n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                            break;
-                        }
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasDiez) + " monedas de 10\n");
-                        break;
-                    }
-                }
-                if (numeroIntroducido % 100 >= 50) {
-                    if (residuoCien % 50 >= 20) {
-                        if (residuoCiencuenta % 20 >= 10) {
-                            if (residuoVente % 10 >= 5) {
-                                txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100\n" + String.valueOf(billetesCincuenta) + " Papeleta de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " Moneda de 10\n" + String.valueOf(monedasCinco) + " Moneda de 5");
-                                break;
-                            }
-                            txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50\n" + String.valueOf(billetesVente) + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " Moneda de 10");
-                            break;
-                        }
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50\n" + String.valueOf(billetesVente) + " Papeletas de 20");
-                        break;
-                    }
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50");
-                    break;
-                }
-                if (residuoCien / 50 < 1 && residuoCiencuenta >= 20) {
-
-                    if (residuoCiencuenta / 20 >= 1 && residuoCiencuenta % 20 >= 5 && residuoCiencuenta % 20 < 10) {
-
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                        break;
-                    }
-
-                    if (residuoCiencuenta % 20 >= 10) {
-
-                        if (residuoVente % 10 >= 5) {
-
-                            txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasDiez) + " monedas de 10\n" + String.valueOf(monedasCinco) + " Monedas de 5");
-                            break;
-                        }
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100 \n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasDiez) + " monedas de 10\n");
-                        break;
-                    }
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100\n" + String.valueOf(billetesVente) + " Papeletas de 20 \n");
-                    break;
-                }
-
-                if (residuoCiencuenta / 20 < 1 && residuoVente >= 10) {
-
-                    if (residuoVente % 10 >= 5) {
-
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " papeletas de 100 \n" + String.valueOf(monedasDiez) + " monedas de 10 \n" + String.valueOf(monedasCinco) + " monedas de 5");
-                        break;
-                    }
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " papeletas de 100 \n" + String.valueOf(monedasDiez) + " monedas de 10 \n");
-                    break;
-                }
-
-                if (residuoVente / 10 < 1 && residuoDiez >= 5) {
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " papeletas de 100\n" + String.valueOf(monedasCinco) + " monedas de 5");
-                    break;
-                }
-
-                txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCien) + " Papeletas de 100");
-            }
-            else if (numeroIntroducido / 100 < 1 && numeroIntroducido >= 50) {
-                if (residuoCien / 50 >= 1 && residuoCien % 50 <= 9 && residuoCien % 50 != 0) {
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                    break;
-                }
-                if (residuoCien / 50 >= 1 && residuoCiencuenta / 20 >= 1) {
-                    if (residuoVente >= 10) {
-                        if (residuoDiez >= 5 && residuoDiez < 10) {
-                            txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente)
-                                    + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " monedas de 10\n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                            break;
-                        }
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " monedas de 10\n");
-                        break;
-                    }
-                    if (residuoDiez >= 5 && residuoDiez < 10) {
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente)
-                                + " Papeletas de 20\n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                        break;
-                    }
-
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20\n");
-                    break;
-                }
-
-                if (residuoCien / 50 >= 1 && residuoCien % 50 >= 10) {
-                    if (residuoDiez / 5 >= 1) {
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasDiez) + " monedas de 10\n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                        break;
-                    }
-
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(monedasDiez) + " monedas de 10\n");
-                    break;
-                }
-
-                if (residuoCien % 50 >= 20) {
-                    if (residuoCiencuenta % 20 >= 10) {
-                        if (residuoVente % 10 >= 5) {
-                            txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " Monedas de 10\n" + String.valueOf(monedasCinco) + " Monedas de 5");
-                            break;
-                        }
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20\n" + String.valueOf(monedasDiez) + " Monedas de monedasDiez");
-                        break;
-                    }
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50 \n" + String.valueOf(billetesVente) + " Papeletas de 20");
-                    break;
-                }
-                txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesCincuenta) + " Papeletas de 50");
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
                 break;
-            }
-            else if (numeroIntroducido / 50 < 1 && numeroIntroducido >= 20) {
-                if (residuoCiencuenta / 20 >= 1 && residuoCiencuenta % 20 >= 5 && residuoCiencuenta % 20 < 10) {
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasCinco) + " monedas de 5\n");
-                    break;
-                }
-                if (residuoCiencuenta % 20 >= 10) {
-                    if (residuoVente % 10 >= 5) {
-                        txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasDiez) + " monedas de 10\n" + String.valueOf(monedasCinco) + " Monedas de 5");
-                        break;
+
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                float valueX = x2 - x1;
+                float valueY = y2 - y1;
+
+                if (Math.abs(valueX) > MIN_DISTANCE){
+                    if (x2>x1){
+                        MainActivity.this.finish();
+                        System.exit(0);
+                    }else {
+                        txtResult.setText("");
+                        txtNumero.setText("");
                     }
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesVente) + " Papeletas de 20 \n" + String.valueOf(monedasDiez) + " monedas de 10\n");
-                    break;
                 }
-                txtResult.setText("Usted tiene: \n\n" + String.valueOf(billetesVente) + " Papeletas de 20");
-                break;
-            }
-            else if (numeroIntroducido / 20 < 1 && numeroIntroducido >= 10) {
-                if (residuoVente % 10 >= 5) {
-                    txtResult.setText("Usted tiene: \n\n" + String.valueOf(monedasDiez) + " monedas de 10 \n" + String.valueOf(monedasCinco) + " monedas de 5");
-                    break;
-                }
-                txtResult.setText("Usted tiene: \n\n" + String.valueOf(monedasDiez) + " Monedas de 10");
-                break;
-            }
-            else if (numeroIntroducido / 10 < 1 && numeroIntroducido >= 5) {
-                txtResult.setText("Usted tiene: \n\n" + String.valueOf(monedasCinco) + " monedas de 5");
-                break;
-            }
-            else {
-                txtResult.setText("ERROR_DE_CAMBIO   'El monto minimo es 5'");
-            }
         }
-    }*/
+
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+}
 
 
 
